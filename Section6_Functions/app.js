581b1e24f70ec7b35d3bafa3b1aecfbffefdbe79
Semin Lim <dani416@gmail.com>
2024-06-30 16:17:21 +0900

    Section 6 132 (코드 게임 로직 구현하기)

--- Section6_Functions/app.js
+++ Section6_Functions/app.js
@@ -4,6 +4,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS'
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
@@ -18,6 +21,31 @@ const getPlayerChoice = function () {
         return DEFAULT_USER_CHOICE;
     }
     return selection;
+}
+
+const getComputerChoice = function() {
+    const randomValue = Math.random();
+    if(randomValue < 0.34) {
+        return ROCK;
+    } else if (randomValue <0.67) {
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+}
+
+const getWinner = function (cChoice, pChoice) {
+    if(cChoice === pChoice) {
+        return RESULT_DRAW;
+    } else if (
+        cChoice === ROCK && pChoice === PAPER || 
+        cChoice === PAPER && pChoice === SCISSORS ||
+        cChoice === SCISSORS && pChoice === ROCK
+    ) {
+        return RESULT_PLAYER_WINS;
+    } else {
+        return RESULT_COMPUTER_WINS;
+    }
 }
 
 // const start = function () {
@@ -42,7 +70,9 @@ startGameBtn.addEventListener('click',function startGame(){
     }
     gameIsRunning = true;
     console.log('Game is starting...');
-    const playerSelection = getPlayerChoice();
-    console.log(playerSelection);
+    const playerChoice = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(computerChoice,playerChoice);
+    console.log(winner);
 }); //indirect execution
 // 익명 함수에 이름을 지정하면 디버깅할 때 훨씬 편리하다.
