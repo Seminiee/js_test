ab9905b1b3a083360e321bd5ae45fc671cd3ca49
Semin Lim <dani416@gmail.com>
2024-06-30 16:02:58 +0900

    Section 6 131 (프로젝트 작업하기: 게임에 사용자의 선택 추가하기)

--- Section6_Functions/app.js
+++ Section6_Functions/app.js
@@ -1,5 +1,25 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS'
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}`,'').toUpperCase();
+    if (
+        selection !== ROCK && 
+        selection !== PAPER && 
+        selection !== SCISSORS
+    ) {
+        alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection;
+}
+
 // const start = function () {
 //     console.log('Game is starting...');
 // };
@@ -17,6 +37,12 @@ const startGameBtn = document.getElementById('start-game-btn');
 // console.log(person.name);
 
 startGameBtn.addEventListener('click',function startGame(){
-    console.log('Game is starting...',age);
+    if (gameIsRunning) {
+        return;
+    }
+    gameIsRunning = true;
+    console.log('Game is starting...');
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
 }); //indirect execution
 // 익명 함수에 이름을 지정하면 디버깅할 때 훨씬 편리하다.
