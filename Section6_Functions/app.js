de21f5ad52032af2651c514ceb72a0917c2daa73
Semin Lim <dani416@gmail.com>
2024-06-30 15:49:56 +0900

    Section 6 130 (익명 함수)

--- Section6_Functions/app.js
+++ Section6_Functions/app.js
@@ -1,8 +1,8 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-const start = function () {
-    console.log('Game is starting...');
-};
+// const start = function () {
+//     console.log('Game is starting...');
+// };
 
 // const person = {
 //     greet: function greet() {
@@ -16,4 +16,7 @@ const start = function () {
 
 // console.log(person.name);
 
-startGameBtn.addEventListener('click',start); //indirect execution
+startGameBtn.addEventListener('click',function startGame(){
+    console.log('Game is starting...',age);
+}); //indirect execution
+// 익명 함수에 이름을 지정하면 디버깅할 때 훨씬 편리하다.
