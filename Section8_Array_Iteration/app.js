50935eab8b765f4f0c646c1eb5fa09d6bf47dbdd
Semin Lim <dani416@gmail.com>
2024-07-02 14:07:46 +0900

    Section 8 197 (`push()`, `pop()`, `unshift()`, `shift()` - 요소 추가 & 제거하기)

--- Section8_Array_Iteration/app.js
+++ Section8_Array_Iteration/app.js
@@ -7,22 +7,34 @@ console.log(numbers);
 // const yetMoreNumbers = Array.of(1, 2);
 // console.log(yetMoreNumbers);
 
-const listItems = document.querySelectorAll('li'); // NodeList
-console.log(listItems);
+// const listItems = document.querySelectorAll('li'); // NodeList
+// console.log(listItems);
 
-const newArray = Array.from('Hi!');
-console.log(newArray);
+// const newArray = Array.from('Hi!');
+// console.log(newArray);
 
-const arrayListItems = Array.from(listItems);
-console.log(arrayListItems);
+// const arrayListItems = Array.from(listItems);
+// console.log(arrayListItems);
 
-const hobbies = ['Cooking', 'Sports'];
-const personalData = [30, 'Max', {moreDetail: []}]; // 여러 자료구조를 가질 수 있다.
+// const hobbies = ['Cooking', 'Sports'];
+// const personalData = [30, 'Max', {moreDetail: []}]; // 여러 자료구조를 가질 수 있다.
 
-const analyticsData = [[1, 1.6], [-5.4, 2.1]]; //다차원 배열
+// const analyticsData = [[1, 1.6], [-5.4, 2.1]]; //다차원 배열
 
-for (const data of analyticsData) {
-    for(const dataPoint of data) {
-        console.log(dataPoint);
-    }
-}
+// for (const data of analyticsData) {
+//     for(const dataPoint of data) {
+//         console.log(dataPoint);
+//     }
+// }
+
+const hobbies = ['Sports', 'Cooking', ];
+hobbies.push('Reading'); //O(1)
+hobbies.unshift('Coding'); // O(n)
+const poppedValue = hobbies.pop(); // O(1)
+hobbies.shift(); // O(n)
+console.log(hobbies);
+
+hobbies[1] ='Coding';
+hobbies[5] = 'Reading'; // rarely used
+console.log(hobbies);
+
