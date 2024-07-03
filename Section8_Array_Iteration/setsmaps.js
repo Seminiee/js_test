2c0d7abffdadefb6d1fb2a4693be0471110c8254
Semin Lim <dani416@gmail.com>
2024-07-03 15:07:20 +0900

    Section 8 206 (Maps 작업하기)

--- Section8_Array_Iteration/setsmaps.js
+++ Section8_Array_Iteration/setsmaps.js
@@ -10,3 +10,32 @@
 // for (const value of ids.values()) {
 //     console.log(value);
 // }
+
+const person1 = {name: 'Max'};
+const person2 = {name: 'Manuel'};
+
+// const personData = new Map([['key', 'some value']]);
+const personData = new Map([[person1, [{date: 'yesterday', price: 10}]]]);
+
+personData.set(person2, [{date: 'two weeks ago', price: 100}]);
+console.log(personData);
+console.log(personData.get(person1));
+/** you DON'T have to do that:
+ * const person1 = {id: 'p1',...}
+ * const personData = {p1: [...]};
+ * console.log(personData[person.id]);
+ */
+
+for (const [key, value] of personData.entries()) { // key - value pair
+    console.log(key, value);
+}
+
+for (const key of personData.keys()) {
+    console.log(key);
+}
+
+for (const value of personData.values()) {
+    console.log(value);
+}
+
+console.log(personData.size); // size는 property
