a972052286cd86845b35e76b0527e464444c9e0c
Semin Lim <dani416@gmail.com>
2024-07-05 12:02:22 +0900

    Section 9 218 & 219 (프로퍼티 유형 & 프로퍼티 순서)

--- Section9_Objects/assets/scripts/objects.js
+++ Section9_Objects/assets/scripts/objects.js
@@ -4,22 +4,31 @@
 const movieList = document.getElementById('movie-list');
 
 movieList.style['backgroundColor'] = 'red';
-movieList.style.display = 'block';
+movieList.style.display = 'block'
+// 숫자형으로만 키를 가지는 객체는 키에 해당하는 숫자의 오름차 순으로 객체가 정렬됨.
+
+const userChosenKeyName = 'level';
 
 const person = {
     'first name' :'Max',
     age: 30,
     hobbies: ['Sports','Cooking'],
+    [userChosenKeyName]: '...', // dynamically set to the Object
     greet: function() {
         alert('Hi there!');
-    }
+    },
+    1.5 : 'hello' // 숫자형 키는 string으로 강제 변환, 음수는 사용 불가
 };
-console.log(person['first name'])
+// console.log(person['first name'])
+console.log(person[1.5]); // 자주 발생하지는 않는다.
+
+const keyName = 'first name';
+// console.log(person[keyName]); // dynamically access to the Object
 // ...
 // 프로퍼티가 존재하든 존재하지 않든 추가할 수 있다.
 
 // person.age = 31
-// elete person.age; // 프로퍼티 삭제
+// delete person.age; // 프로퍼티 삭제 ->더 이상 해당 프로퍼티를 사용하지 않겠다.
 
 /**
  * 객체에서 property 
