66759f01accd665a9eae0e6e67de3d8a032f3fc6
Semin Lim <dani416@gmail.com>
2024-07-05 11:30:12 +0900

    Section 9 216 (프로퍼티 추가, 수정 & 삭제)

--- Section9_Objects/assets/scripts/objects.js
+++ Section9_Objects/assets/scripts/objects.js
@@ -7,4 +7,20 @@ const person = {
     }
 };
 
-person.greet();
+// ...
+// 프로퍼티가 존재하든 존재하지 않든 추가할 수 있다.
+
+// person.age = 31
+// elete person.age; // 프로퍼티 삭제
+
+/**
+ * 객체에서 property 
+ * -> undefined: 해당 프로퍼티 없음
+ * -> null: 해당 프로퍼티는 존재, 그러나 값이 정해져 있지 않음(개발자가 설정한 것임) 
+ * */ 
+
+person.age = null;
+person.isAdmin = true;
+console.log(person.age);    
+
+// person.greet();
