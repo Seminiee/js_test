730307d7442d118967f81a76a9a0d6fb551f2f1a
Semin Lim <dani416@gmail.com>
2024-07-08 15:46:01 +0900

    Section 10 241 (OOP 코드로 시작하기)

---
+++ Section10_OOP/assets/scripts/app.js
@@ -0,0 +1,42 @@
+const productList = {
+    products: [
+        {
+            title: 'A Pillow', 
+            imageUrl:
+             'https://m.media-amazon.com/images/I/61y6iRvb-WL._AC_UF894,1000_QL80_.jpg',
+            price: 19.99,
+            description: 'A soft pillow!'
+        },
+        {
+            title: 'A Carpet', 
+            imageUrl:
+             'https://thecarpetier.sg/cdn/shop/products/p-8041-persian-carpetcar-cash-8041-120-493235.webp?v=1710842713&width=1946',
+            price: 89.99,
+            description: 'A carpet which you might like - or not.'
+        },
+    ],
+    render() {
+        const renderhook = document.getElementById('app');
+        const prodList = document.createElement('ul');
+        prodList.className = 'product-list';
+        for( const prod of this.products) {
+            const prodEl = document.createElement('li');
+            prodEl.className = 'product-item';
+            prodEl.innerHTML = `
+                <div>
+                    <img src="${prod.imageUrl}" alt="${prod.title}" >
+                    <div class="product-item__content">
+                        <h2>${prod.title}</h2>
+                        <h3>\$${prod.price}</h3>
+                        <p>${prod.description}</p>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
+            `;
+            prodList.append(prodEl)
+        }
+        renderhook.append(prodList);
+    }
+};
+
+productList.render();
