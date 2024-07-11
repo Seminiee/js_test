7791db3fb2a1fb7eb32dd70aafe1cad5bf6b75b1
Semin Lim <dani416@gmail.com>
2024-07-11 21:17:49 +0900

    Section 11 268 (프로토타입 소개) Section 11 269 (프로토타입 요약)

--- Section11_constructor_function_prototype/app.js
+++ Section11_constructor_function_prototype/app.js
@@ -1,24 +1,53 @@
 // Person Class
-// class Person {
-//     name = 'Max';
+class AgedPerson {
+    printAge() {
+        console.log(this.age)
+    }
+}
+class Person extends AgedPerson{
+    name = 'Max';
 
-//     constructor() {
-//         this.age = 30;
-//     }
+    constructor() {
+        super();
+        this.age = 30;
+    }
 
-//     greet() {
-//         console.log('Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.');
-//     }
-// }
-
-// Person Constructor Function with using "new" keyword -> class가 생성자 함수의 문법적 설탕이라고도 볼 수 있으나, 이면에서는 class가 더 많은 것을 한다
-function Person() {
-    this.age = 30;
-    this.name = 'Max';
-    this.greet = function() {
+    greet() {
         console.log('Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.');
     }
 }
 
-const person = new Person();
-person.greet();
+// Person Constructor Function with using "new" keyword -> class가 생성자 함수의 문법적 설탕이라고도 볼 수 있으나, 이면에서는 더 많은 것을 한다
+// function Person() {
+//     this.age = 30;
+//     this.name = 'Max';
+//     this.greet = function() {
+//         console.log('Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.');
+//     }
+// }
+
+// Person.prototype = {
+//     printAge() {
+//         console.log(this.age);
+//     }
+// }
+
+Person.prototype.printAge = function () {
+    console.log(this.age);
+}
+
+console.dir(Person);
+
+const p = new Person();
+p.greet();
+p.printAge();
+console.log(p.__proto__);
+console.log(p.toString());
+const p2 = new p.__proto__.constructor();
+// console.log(person.toString()); // prototype을 기본 클래스 라고 생각해도 큰 차이가 없다.
+console.log(p2); 
+
+// __: double underscore , dunder
+
+
+ 
