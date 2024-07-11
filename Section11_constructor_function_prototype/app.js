a3e85b42d4e20f3335ce5866de254256283585aa
Semin Lim <dani416@gmail.com>
2024-07-11 18:47:51 +0900

    Section 11 267 (생성자 함수 vs 클래스 & `new` 이해하기)

--- Section11_constructor_function_prototype/app.js
+++ Section11_constructor_function_prototype/app.js
@@ -11,7 +11,7 @@
 //     }
 // }
 
-// Person Constructor Function
+// Person Constructor Function with using "new" keyword -> class가 생성자 함수의 문법적 설탕이라고도 볼 수 있으나, 이면에서는 class가 더 많은 것을 한다
 function Person() {
     this.age = 30;
     this.name = 'Max';
